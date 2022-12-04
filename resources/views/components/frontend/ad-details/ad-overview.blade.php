021416aa490bf1ffb79b7c901606eae9f5e2f47b
mahi_rbs <maidul.tech@com>
2022-12-04 17:28:13 +0600

    brand position

--- resources/views/components/frontend/ad-details/ad-overview.blade.php
+++ resources/views/components/frontend/ad-details/ad-overview.blade.php
@@ -1,14 +1,13 @@
 <div>
-    
+    @if($ad->brand)
+    <h2 class="text--body-1">{{ __('overview') }}</h2>
     <ul class="overview-details">
-       
-        @if($ad->brand)
-        <h2 class="text--body-1">{{ __('overview') }}</h2>
+        
         <li class="overview-details__item">
             <span class="text--body-3 title">{{ __('Brand Name') }}:</span>
             <span class="text--body-3 info">{{ $ad->brand->name ?? '' }}</span>
         </li>
-        @endif
         
     </ul>
+    @endif
 </div>
