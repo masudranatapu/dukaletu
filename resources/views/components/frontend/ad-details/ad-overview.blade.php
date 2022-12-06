4df7fabf8460f0a28661319a0503f70095643e7b
rabin mia <rabinmia7578@gmail.com>
2022-12-06 10:26:43 +0600

    //

--- resources/views/components/frontend/ad-details/ad-overview.blade.php
+++ resources/views/components/frontend/ad-details/ad-overview.blade.php
@@ -1,13 +1,13 @@
-<div>
+<div class="pb-2">
     @if($ad->brand)
-    <h2 class="text--body-1">{{ __('overview') }}</h2>
+    <h2 class="share-content__title text--body-3">{{ __('overview') }}</h2>
     <ul class="overview-details">
         
         <li class="overview-details__item">
             <span class="text--body-3 title">{{ __('Brand Name') }}:</span>
             <span class="text--body-3 info">{{ $ad->brand->name ?? '' }}</span>
         </li>
-        
+    
     </ul>
     @endif
 </div>
