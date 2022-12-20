64e86e2a3ee00b2e0dfe026afec56ea96bf9dedc
Masud Rana Tapu <91066147+masudranatapu@users.noreply.github.com>
2022-12-20 12:55:13 +0600

    done

--- resources/views/components/footer/footer-app.blade.php
+++ resources/views/components/footer/footer-app.blade.php
@@ -1,6 +1,6 @@
 <div class="col-xl-4 col-lg-6">
     @if ($mobile_setting->ios_download_url || $mobile_setting->android_download_url)
-        <h2 class="footer__title text--body-2-600">{{ __('download_our_app') }}</h2>
+        <h2 class="footer__title text--body-2-600 footer__mobile-app-text">{{ __('download_our_app') }}</h2>
     @endif
     <div class="footer__mobile-app">
         @if ($mobile_setting->android_download_url)
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,7 +8,7 @@
 
 @section('meta')
     @php
-    $data = metaData('home');
+        $data = metaData('home');
     @endphp
 
     <meta name="title" content="{{ $data->title }}">
@@ -34,7 +34,6 @@
         $sliders = DB::table('sliders')->where('status', 1)->get();
     @endphp
 
-    <!-- banner section start  --> 
     <div class="banner banner--two text-center" style="background-image:url('{{ $cms->home_main_banner }}');">
         <div class="container">
             <div class="row d-none d-sm-block">
@@ -64,7 +63,6 @@
         </div>
     </div>
 
-    <!-- banner section end   -->
     <div class="main_content mb-5">
         <div class="container">
             <div class="row g-4">
@@ -73,11 +71,9 @@
                         <div class="category_wrapper">
                             <ul class="category-menu">
                                 @foreach ($categories as $category)
-                                    <form method="GET" action="{{ route('frontend.adlist.search') }}"
-                                        id="adFilterForm2" class="d-none">
+                                    <form method="GET" action="{{ route('frontend.adlist.search') }}" id="adFilterForm2" class="d-none">
                                         <input type="hidden" name="category" value="" id="adFilterInput2">
                                     </form>
-
                                     <li class="category-menu__dropdown__item">
                                         <a href="javascript:void(0)"
                                         onclick="adFilterFunctionTwo('{{ $category->slug }}')"
@@ -113,39 +109,33 @@
                                         @endif
                                     </li>
                                  @endforeach
-
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-9">
+
                     <div class="sidebar_right">
-
-                     <!-- Slider banner -->
-                     <div class="banner_carousel mb-4">
-                         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                              <div class="carousel-inner">
-                                @foreach($sliders as $key => $slider)
-                                    <div class="carousel-item @if($key == 0) active @endif">
-                                        <div class="banner_img">
-                                            <img src="{{ asset($slider->image) }}" class="d-block w-100" alt="{{$slider->name}}">
+                        <div class="banner_carousel mb-4">
+                            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    @foreach($sliders as $key => $slider)
+                                        <div class="carousel-item @if($key == 0) active @endif">
+                                            <div class="banner_img">
+                                                <img src="{{ asset($slider->image) }}" class="d-block w-100" alt="{{$slider->name}}">
+                                            </div>
                                         </div>
-                                    </div>
-                                @endforeach
-                              </div>
-                              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg>
-                              </button>
-                              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/></svg>
-                              </button>
+                                    @endforeach
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/></svg>
+                                </button>
+                            </div>
                         </div>
-                     </div>
-                     <!-- Slider banner -->
 
-
-
-                      <!-- Poupular category Section start  -->
                         <section class="section popular-category d-block d-sm-none" style="padding-top: 0px !important; padding-bottom: 25px;">
                             <h2 class="text--heading-1 section__title">
                                         {{ __('popular_category') }}
@@ -166,28 +156,13 @@
                                                 <a href="javascript:void(0)" onclick="adFilterFunction('{{ $category->slug }}')">
                                                     <h5 class="text--body-2 category-card__title"> {{ $category->name }} </h5>
                                                 </a>
-                                                <!-- <div class="category-card__view">
-                                                    <span class="first view-number"> {{ $category->ad_count ?? 0 }}
-                                                        {{ __('ads') }}</span>
-                                                    <a href="javascript:void(0)" onclick="adFilterFunction('{{ $category->slug }}')"
-                                                        class="second view-btn">
-                                                        {{ __('view_ads') }}
-                                                        <span class="icon">
-                                                            <x-svg.right-arrow-icon stroke="#3db83a" />
-                                                        </span>
-                                                    </a>
-                                                </div> -->
                                             </div>
                                         </div>
                                     @endforeach
                                 </div>
                             </div>
                         </section>
-                        <!-- Poupular category Section end  -->
 
-
-
-                        <!-- recent-post section start  -->
                         @if ($settings->featured_ads_homepage)
                             <section class="section recent-post">
                                     <h2 class="text--heading-1 section__title">
@@ -200,23 +175,9 @@
                                             <x-no-data-found />
                                         @endforelse
                                     </div>
-                                    {{--
-                                        @if (count($recommendedAds) > 0)
-                                            <div class="recent-post__btn">
-                                                <a href="{{ route('frontend.adlist') }}" class="btn">
-                                                    {{ __('view_all') }}
-                                                    <span class="icon--right">
-                                                        <x-svg.right-arrow-icon />
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        @endif
-                                    --}}
                             </section>
                         @endif
-                        <!-- recent-post section end -->
 
-                        <!-- recent-post section start  -->
                         @if ($settings->regular_ads_homepage)
                             <section class="section recent-post">
                                     <h2 class="text--heading-1 section__title">
@@ -229,166 +190,21 @@
                                             <x-no-data-found />
                                         @endforelse
                                     </div>
-                                    {{--
-                                        @if (count($latestAds) > 0)
-                                            <div class="recent-post__btn">
-                                                <a href="{{ route('frontend.adlist') }}" class="btn">
-                                                    {{ __('view_all') }}
-                                                    <span class="icon--right">
-                                                        <x-svg.right-arrow-icon />
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        @endif
-                                     --}}
                             </section>
                         @endif
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-
-    <!-- recent-post section end -->
-
-    <!-- top-category section start  -->
-
-    <!-- <section class="section top-category bgcolor--gray-10">
-        <div class="container">
-            <h2 class="text--heading-1 section__title">
-                {{ __('top_category') }}
-            </h2>
-            <div class="row">
-                @forelse ($topCategories as $category)
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="categorylist-card">
-                            <div class="categorylist-card__top">
-                                <div class="categorylist-card__top-left">
-                                    <h2 class="categorylist-card__title text--body-2-600">
-                                        <a
-                                            href="{{ route('frontend.adlist', ['category' => $category->slug]) }}">{{ $category->name }}</a>
-                                    </h2>
-                                    <span
-                                        class="categorylist-card__item-available">({{ $category->ad_count ?? 0 }})</span>
-                                </div>
-                                <div class="categorylist-card__top-right">
-                                    <div class="categorylist-card__icon">
-                                        <i class="{{ $category->icon }}" style="font-size: 55px"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="categorylist-card__bottom">
-                                <ul class="categorylist-card__list">
-
-                                    {{-- Filter Form 1 --}}
-                                    <form method="GET" action="{{ route('frontend.adlist.search') }}" id="adFilterForm"
-                                        class="d-none">
-                                        <input type="hidden" name="subcategory[]" value="" id="adFilterInput">
-                                    </form>
-                                    @forelse ($category->subcategories as $subcategory)
-                                        <li class="categorylist-card__list-item">
-                                            <a href="javascript:void(0)"
-                                                onclick="adFilterFunction('{{ $subcategory->slug }}')"
-                                                class="categorylist-card__list-link text--body-3">
-                                                <span class="icon">
-                                                    <x-svg.right-regular-icon />
-                                                </span>
-                                                {{ $subcategory->name }}
-                                            </a>
-                                        </li>
-                                    @empty
-                                        <div class="text-center">
-                                            {{ __('no_subcategory_found') }}
-                                        </div>
-                                    @endforelse
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                @empty
-                    <x-no-data-found />
-                @endforelse
-            </div>
-        </div>
-    </section> -->
-    <!-- top-category section end  -->
-
-    {{-- <!-- popular-loc section start  -->
-   <!--  <section class="section popular-location">
-        <div class="container">
-            <h2 class="text--heading-1 section__title">
-                {{ __('popular_location') }}
-            </h2>
-            <div class="row">
-                @forelse ($topCountry as $country)
-                    <div class="col-xl-3 col-md-6">
-                        <x-frontend.location.single-popular-location :country="$country">
-                        </x-frontend.location.single-popular-location>
-                    </div>
-                @empty
-                    <x-no-data-found />
-                @endforelse
-            </div>
-        </div>
-    </section> -->
-    <!-- popular-loc section end --> --}}
-
     <x-frontend.counter :totalAds="$totalAds" :verifiedUser="$verified_users" :proMember="$pro_members_count" :country="$country_location"></x-frontend.counter>
-
-    <!-- download section start  -->
-    {{--  
-    @if ($mobile_setting->ios_download_url || $mobile_setting->android_download_url)
-        <section class="download section pb-lg-0">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6">
-                        <img src="{{ $cms->home_mobile_app_banner }}" class="download__img-content w-100" />
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="download__content">
-                            <h2 class="download__title text--heading-1">{{ __('download_our_mobile_app') }}</h2>
-                            <p class="download__brief text--body-2">
-                                {{ $cms->download_app }}
-                            </p>
-                            <div class="download__apps-store">
-                                @if ($mobile_setting->android_download_url)
-                                    <a target="_blank" href="{{ asset($mobile_setting->android_download_url) }}"
-                                        class="app">
-                                        <div class="app-logo">
-                                            <x-svg.google-play-icon />
-                                        </div>
-                                        <div class="app-info">
-                                            <h5 class="text--body-5">{{ __('get_it_now') }}</h5>
-                                            <h2 class="text--body-3-600">{{ __('google_play') }}</h2>
-                                        </div>
-                                    </a>
-                                @endif
-
-                                @if ($mobile_setting->ios_download_url)
-                                    <a target="_blank" href="{{ asset($mobile_setting->ios_download_url) }}"
-                                        class="app">
-                                        <div class="app-logo">
-                                            <x-svg.apple-icon />
-                                        </div>
-                                        <div class="app-info">
-                                            <h5 class="text--body-5">{{ __('get_it_now') }}</h5>
-                                            <h2 class="text--body-3-600">{{ __('app_store') }}</h2>
-                                        </div>
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    @endif
-    --}}
 
     @if ($newsletter_enable)
         @include('layouts.frontend.partials.newsletter')
     @endif
+
 @endsection
 
 @section('frontend_script')
@@ -413,7 +229,5 @@
             $('#adFilterForm3').submit();
         }
     </script>
-
-
 
 @endsection
